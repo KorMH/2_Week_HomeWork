8dc3e59ee50002d6165d8e986ed20cf759cc0744
KorMH <105302873+KorMH@users.noreply.github.com>
2023-07-05 14:10:01 +0900

    Update SignupRequestDto.java
    최종

--- src/main/java/com/board/inoboard/dto/SignupRequestDto.java
+++ src/main/java/com/board/inoboard/dto/SignupRequestDto.java
@@ -9,8 +9,12 @@ import lombok.Setter;
 @Getter
 @Setter
 public class SignupRequestDto {
+    @Column(unique = true)
+    @Pattern(regexp = "^[a-z0-9]{4,10}$",message = "4~10자리 소문자와숫자만 넣으세요")
     private String username;
+    @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
     private String password;
+    @Email
     private String email;
 
 
